830a7744b63a25dc8607512e680732d1bd6fd987
Staick <staick@163.com>
2022-08-30 17:35:41 +0800

    chapter 08 first commit

---
+++ chapter_08/ex8_1.py
@@ -0,0 +1,5 @@
+# 8-1. Message
+def display_message():
+    print("I am learning about function in this chapter.")
+
+display_message()
---
+++ chapter_08/ex8_2.py
@@ -0,0 +1,5 @@
+# 8-2. Favorite Book
+def favorite_book(book):
+    print(f"One of my favorite books is {book}.")
+
+favorite_book('Alice in Wonderland')
