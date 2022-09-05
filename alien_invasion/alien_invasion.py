fb8ebb92f9e24bdcbcb0bd69e108f5551776d6b1
Staick <staick@163.com>
2022-09-05 20:02:18 +0800

    fix bugs and implement responding to a keypress.

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -25,11 +25,16 @@ class AlienInvasion:
             self._check_events()
             self._update_screen()
 
-    def _ckeck_events(self):
+    def _check_events(self):
         """Respond to keypresses and mouse events."""
         for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RIGHT:
+                        # Move the ship to the right.
+                        self.ship.rect.x += 1
+                
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
